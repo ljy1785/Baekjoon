bf842b439c678fb3cab6d907e2e7256c1c1da8c1
ljy1785 <108108454+ljy1785@users.noreply.github.com>
2026-03-20 11:43:55 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ Python/백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,20 @@
+import sys
+n = int(input())
+stack = []
+for i in range(n):
+  command = sys.stdin.readline().split()
+  if command[0] == 'push':
+    stack.append(command[1])
+  elif command[0] == 'pop':
+    if not stack: print(-1)
+    else:
+      print(stack.pop(-1))
+  elif command[0] == 'size':
+    print(len(stack))
+  elif command[0] == 'empty':
+    if len(stack) == 0: print(1)
+    else: print(0)
+  else: #command == 'top'
+    if len(stack) == 0: print(-1)
+    else : print(stack[-1])
+
