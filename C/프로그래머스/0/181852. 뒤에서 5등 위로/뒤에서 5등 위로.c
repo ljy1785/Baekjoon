3ada95e67160c5d678364ef66a42ae651a8b581c
ljy1785 <108108454+ljy1785@users.noreply.github.com>
2026-05-06 22:15:30 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ C/프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+int compare(const void *a, const void *b){
+    return (*(int*)a-*(int*)b);
+}
+
+// num_list_len은 배열 num_list의 길이입니다.
+int* solution(int num_list[], size_t num_list_len) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    qsort(num_list, num_list_len, sizeof(int), compare);
+    int answer_len = num_list_len-5;
+    int* answer = (int*)malloc(sizeof(int)*answer_len);
+    for (int i = 0; i< answer_len; i++) answer[i] = num_list[i+5];
+    return answer;
+}
